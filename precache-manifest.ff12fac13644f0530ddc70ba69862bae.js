ebe60557976c69ecc45b9d021ce826fb5a7d8571
shunwuyu <shunwu2001@163.com>
2018-11-28 10:25:52 +0800

    Updates

---
+++ precache-manifest.ff12fac13644f0530ddc70ba69862bae.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d6d0aa42199878ab9859",
+    "url": "/react-tutorial/static/js/runtime~main.d6d0aa42.js"
+  },
+  {
+    "revision": "7e6a1a79d95f7e35c96d",
+    "url": "/react-tutorial/static/js/main.7e6a1a79.chunk.js"
+  },
+  {
+    "revision": "65ce664db22ebc0891b6",
+    "url": "/react-tutorial/static/js/1.65ce664d.chunk.js"
+  },
+  {
+    "revision": "7e6a1a79d95f7e35c96d",
+    "url": "/react-tutorial/static/css/main.5cf0d0e8.chunk.css"
+  },
+  {
+    "revision": "cf5a6afafa907a543a85333ac78374d9",
+    "url": "/react-tutorial/index.html"
+  }
+];
